1c57cf507d0de33e1077f13a84e8be36e46de192
Tejas <tejastarghale@gmail.com>
2023-11-05 22:04:24 +0530

    I have made change in Database configuration like introduced SQL Configuration

--- Shopping/Shopping/settings.py
+++ Shopping/Shopping/settings.py
@@ -51,8 +51,12 @@ WSGI_APPLICATION = 'Shopping.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shop',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',   # Set to the host where your MySQL server is running
+        'PORT': '3306',        # Default MySQL port
     }
 }
 
